617514a96d4ff0f01328ba34b88fca479113e0bc
may <mzhboy@gmail.com>
2019-06-05 04:35:49 +0800

    small improvement

--- build_hpl.sh
+++ build_hpl.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+pushd .
+HPL=hpl-2.3
 sudo apt install libopenblas-dev libatlas-base-dev libmpich-dev gfortran
-wget http://www.netlib.org/benchmark/hpl/hpl-2.3.tar.gz
-tar -zxvf hpl-2.3.tar.gz
-cd hpl-2.3
+wget http://www.netlib.org/benchmark/hpl/${HPL}.tar.gz
+tar -zxvf ${HPL}.tar.gz
+cd $HPL
 ./configure && \
 make && make install-strip
+popd
