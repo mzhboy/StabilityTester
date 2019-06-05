2ecc72ff9c6ad91067abef714bee57fe61ef9092
may <mzhboy@gmail.com>
2019-06-06 03:24:49 +0800

    add simple script

---
+++ runbench.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+count=10
+if [[ $# -gt 0 ]];then
+	if [[ $1 -gt 1 ]] ;then
+		count=$1
+	fi
+fi
+
+pushd .
+cd $(dirname $0)
+for i in `seq 1 $count`;
+do 
+	echo ==== $i 
+	bash stabilityTester.sh
+done
+popd
